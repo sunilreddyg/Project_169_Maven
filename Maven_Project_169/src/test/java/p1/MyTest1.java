47bef66fef5671e788a6c781c2c5f19e3ccd7554
Administrator <Administrator@192.168.2.2>
2019-08-24 13:11:33 +0530

    FirstProject

---
+++ Maven_Project_169/src/test/java/p1/MyTest1.java
@@ -0,0 +1,25 @@
+package p1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class MyTest1 
+{
+  @Test
+  public void tc001()
+  {
+	  Reporter.log("tc001 executed",true);
+  }
+  
+  @Test
+  public void tc002()
+  {
+	  Reporter.log("tc001 executed",true);
+  }
+  
+  @Test
+  public void tc003()
+  {
+	  Reporter.log("tc001 executed",true);
+  }
+}
---
+++ Maven_Project_169/src/test/java/p2/MyTest2.java
@@ -0,0 +1,27 @@
+package p2;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class MyTest2 
+{
+	
+	  @Test
+	  public void tc004()
+	  {
+		  Reporter.log("tc004 executed",true);
+	  }
+	  
+	  @Test
+	  public void tc005()
+	  {
+		  Reporter.log("tc005 executed",true);
+	  }
+	  
+	  @Test
+	  public void tc006()
+	  {
+		  Reporter.log("tc006 executed",true);
+	  }
+
+}
